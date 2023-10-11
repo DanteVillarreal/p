0736c88e415a8e6608ec4fe49c2e768a3dda5b8c
DanteVillarreal <143382343+DanteVillarreal@users.noreply.github.com>
2023-10-11 22:21:51 +0000

    added step 5. did not check errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
+use rand::distributions::{Normal, Distribution};
+use rand::Rng;
 pub mod network;
 
+//step 5
+    fn generate_random_number () -> f64 {
+        //generates random number between 0 and 100 including both 0 and 100
+        rand::thread_rng().gen_range(0..=100);
 
+        //generate random numbr between 0 and 100 excluding 100
+        rand::thread_rng().gen_range(0..=00);
+    }
 
+        //generate random number with mean 0 and std dev of 1
+        let distribution_variable = Normal::new(/*mean*/ , /* std dev */).unwrap();
+        let random_number = distribution_variable.sample(&mut rand::thread_rng());
+
+        //  ^
+        //  |
+        // unwrap()  is unsafe. 
+        // maybe |
+        //       |
+        //       \/
+        let distribution_variable_two_result = Normal::new(/*mean*/ , /* std dev*/ );
+        let distribution_variable_two = match distribution_variable_two_result {
+            Ok(Normal) => Normal;
+            Err(error) => panic!("Problem creating the distribution variable: {:?}", error),
+        };
+        let random number = distribution_variable_two.sample(&mut rand::thread_rng());
+        
 
 
 

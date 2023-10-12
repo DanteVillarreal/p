3752139e85139b50df6ab70c4cef5df24cea9689
DanteVillarreal <Villarreal.DanteJesus@gmail.com>
2023-10-11 22:00:39 -0500

    Fixed errors on step 5

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
+use rand_distr::{StandardNormal, Normal, Distribution};
+
+use rand::Rng;
 pub mod network;
 
+//---step 5 below----///////
+    fn generate_random_number () {
+            //generates random number between 0 and 100 including both 0 and 100
+            rand::thread_rng().gen_range(0..=100);
 
+            //generate random numbr between 0 and 100 excluding 100
+            rand::thread_rng().gen_range(0..=00);
+            
 
+            //generate random number with mean 0 and std dev of 1
+            let standard_distributed_number: f64 = rand::thread_rng().sample(StandardNormal);
+            //or//
+            let desired_mean = 0.0;
+            let desired_std_dev = 1.0;
+            let distribution_type = Normal::new(desired_mean, desired_std_dev).unwrap();
+            let distributed_number = distribution_type.sample(&mut rand::thread_rng());
+            println!("{} is from a standard normal distribution. {} is too",
+                    standard_distributed_number, distributed_number);
 
+            //--dont like unwrap();
+            let desired_mean = 0.0;
+            let desired_std_dev = 1.0;
+            let distribution_type_result = Normal::new(desired_mean, desired_std_dev);
+            let distribution_type = match distribution_type_result {
+                Ok(distribution_type_result) => distribution_type_result,
+                Err(error) => panic!("Could not make a random number in var distribution_type_result. 
+                                            Error code: {:?}", error),
+            };
+            let distributed_number = distribution_type.sample(&mut rand::thread_rng());
+            println!("{} is from a standard normal distribution. {} is too",
+                    standard_distributed_number, distributed_number);
+    }
+///--step 5 above----////////  
 
 
 fn main() {
     println!("Hello, world!");
+    generate_random_number();
 }

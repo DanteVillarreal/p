d1a76518618b84a626083d4b41b7f8de68d327ee
DanteVillarreal <Villarreal.DanteJesus@gmail.com>
2023-10-16 04:17:59 -0500

    I changed up action_functions by making each function return an f64. this will function as the input for the reward function. I will change this though so that I can converge all of my action functions. as in, I will make each action function, at the end of its normal code call the same function that determines the amount gained minus spent, and return that value. Then I would have the reward function take in an f64 from the function at the end of the action_function. I will have to make it unviersal, so I will need massive if statements accounting for which wallet it is in.     I also changed the execute_action_functions by adding some code comments, but thats it. the old code it already had I checked, and it seems as though it should work.

--- src/action_functions.rs
+++ src/action_functions.rs
@@ -8,9 +8,48 @@ pub mod action_functions {
     }
     //all my functions from this point on are going to be in this format:
     //pub fn [coin]_[percentage, floored]_[exchange to buy]_[exchange to sell] () {}
-    pub fn eos_two_coinbase_kraken () {
+    pub fn eos_two_coinbase_kraken () -> f64 {
         //this will go to the [coinbase] api, get how much [2%] of my total usd is in it, buy [eos] from there
         //transfer it to [kraken] wallet api, and immediately sell all of it
+
+
+
+
+
+//------------------I MIGHT PUT THIS IN ANOTHER FUNCTION-----------------------/////
+        //----I WILL HAVE TO CHANGE RETURN TYPE OF ABOVE FUNCTION OF COURSE----//
+        //then it will calculate how much I spent on the coins.
+        //Calculate how much my wallet in the 2nd exchange went up by
+        //then subtract 2nd number - 1st number
+        //to return a f64 that I will then use in my reward function
+
+        //the reason I made it type    Option<f64> is because I don't want to prematurely
+        //    assign a value to it, so if somehow it never gets assigned a value,
+        //    I can then handle the situation.
+        let how_much_i_spent: Option<f64>;
+        match how_much_i_spent {
+            Some(_) => (),
+            None => panic!("how_much_i_spent   is none"),
+        }
+        let how_much_2nd_wallet_changed: Option<f64>;           //same thing as above^^
+        match how_much_2nd_wallet_changed {
+            Some(_) => (),
+            None => panic!("how_much_2nd_wallet_changed    is none"),
+        }
+
+        //can't directly subtract Option types. So I need to handle the possiblity that either of
+        //how_much_i_spent    and    how_much_2nd_wallet_changed    could carry no value
+        //adds another layer of redundancy, which is always good
+        let total_gained = match(how_much_i_spent, how_much_2nd_wallet_changed) {
+            (Some(spent), Some(gained)) => Some(gained-spent),
+            _ => None,
+        };
+
+        //at this point in the code, I have now returned the f64 I waned
+        //now I will use this f64 for the reward function
+
+//-----------ABOVE----------------------------------------------------------///////
+
     }
 
     pub fn eos_two_coinbase_pionex () {
--- src/execute_action_functions.rs
+++ src/execute_action_functions.rs
@@ -4,11 +4,19 @@ pub mod execute_action_functions {
     use crate::network::NeuralNetwork;
     
     pub fn execute_function(epsilon: usize) {
+        
+        //this gives us the index of the q_value we will then use to determine which action_function to execute
         let action_index = neural_network.exploration_or_exploitation(&mut epsilon);
 
-        // Create a vector of function pointers
+        // Create a vector of function pointers so we can then call
         let actions: [fn(); 36] = [two_a_b, two_a_c, /* ... rest of your actions ... */];
-        (actions[index_of_largest_qvalue])();
+
+
+        //this then executes the action
+        //basically:        actions[action_index] points to a function
+        //(actions[aciton_index])();    calls the function that actions[action_index] points to
+        //if:       action_index was 0, it would call the first function
+        (actions[action_index])();
     }
 
 

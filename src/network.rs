cfef785e800cb865cd27d4270d4a75b51bff0e43
DanteVillarreal <143382343+DanteVillarreal@users.noreply.github.com>
2024-03-05 23:48:04 -0600

    starting from 0.  gamma = 0.2. learning_rate = 0.000_001; gradient_cap = +-0.1.     1. removign all replay buffers. 2. making new p, websocket client and last_network layer log files. 3. adding extra space in rewards doc for new run.         JESUS WILLING... THIS IS IT. We got this!

--- src/network.rs
+++ src/network.rs
@@ -1859,7 +1859,8 @@
 			//02/16/24 - changed to 0.5
 			//02/19/24 - changed to 0.4
 			//02/29/24 - changed to 0.3
-			let gamma = 0.2;
+			//03/05/24 - changed to 0.4
+			let gamma = 0.4;
 			//initialize the largest Q-value so far and its index
 			//let mut index_of_largest_qvalue_in_next_state: Option<usize> = None;
 			let mut largest_qvalue_so_far_in_next_state = f64::MIN;
